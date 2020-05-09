8e0c7474b99a7f73ee23d6b3d555890e156fbdea
nikhilcbhat <bhatnikhil314@gmail.com>
2020-05-09 11:12:56 -0700

    Added comments to the view

--- view.py
+++ view.py
@@ -1,18 +1,35 @@
 class TypingView:
 
     def __init__(self):
+        """
+        Initializes a view.
+        """
         return
 
     def show_words(self, active_word, other_words):
+        """
+        Shows the user's active word, 
+        and the other word they have yet to type.
+        """
         return
 
     def recieve_input(self):
+        """
+        Recieves input from the user. 
+        Should have a traceback to the controller's `process_input` function.
+        """
         return
 
     def set_controller(self, controller):
+        """
+        Sets the controller for the view.
+        """
         self.controller = controller
 
     def show_user_result(self, correct_word):
+        """
+        Lets the user know whether the word is correct.
+        """
         return
 
 
