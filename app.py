a13d3e22cd9ee9928b7348bc2fc99c3fa6d47947
nikhilcbhat <bhatnikhil314@gmail.com>
2020-05-09 19:22:26 -0700

    Updated console arguments

--- app.py
+++ app.py
@@ -8,17 +8,12 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Run typing test script")
     parser.add_argument('-d', action='store_true', help="Run in debug mode with simple command-line frontend")
-    parser.add_argument('--time', action='store', type=int, help="Time to run test for")
+    parser.add_argument('-t', '--time', action='store', type=int, help="Time to run test for", default=60)
     args = parser.parse_args()
-  
-    if args.d:
-        v = BasicTypingView()
-    else:
-        v = QtView()
 
+    v = BasicTypingView() if args.d else QtView
     w = document_to_words("file.txt", shuffle=True)
     m = SimpleModel(w)
-    
     c = BasicTypingController(v, m, args.time)
 
     c.run()
