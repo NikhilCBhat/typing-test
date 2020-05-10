8a6828f2ab8489b42122ead56facb760ba97c65c
nikhilcbhat <bhatnikhil314@gmail.com>
2020-05-09 17:37:27 -0700

    Added shuffle option to util

--- util.py
+++ util.py
@@ -1,5 +1,10 @@
-def document_to_words(filepath):
+import random
+
+def document_to_words(filepath, shuffle=False):
     with open(filepath) as f:
         s = f.read()
 
-    return [x for x in s.split(' ') if all([96 < ord(c.lower()) < 124 for c in x ]) and x]
+    words = [x for x in s.split('\n') if all([96 < ord(c.lower()) < 124 for c in x ]) and x]
+    if shuffle:
+        random.shuffle(words)
+    return words
