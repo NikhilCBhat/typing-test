a6f0aa7d2d48de326d8c2003e70f8eec63fbd42d
LucasBriggs154 <lucas.briggs@gmail.com>
2020-05-09 20:08:16 -0600

    added args to script

--- app.py
+++ app.py
@@ -2,11 +2,21 @@ from util import document_to_words
 from controller import BasicTypingController
 from model import SimpleModel
 from view import BasicTypingView, QtView
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run typing test script")
+    parser.add_argument('-d', action='store_true', help="Run in debug mode with simple command-line frontend")
+    args = parser.parse_args()
+
+  
+    if args.d:
+        v = BasicTypingView()
+    else:
+        v = QtView()
+
     w = document_to_words("file.txt", shuffle=True)
     m = SimpleModel(w)
-    v = BasicTypingView()
     c = BasicTypingController(v, m)
 
     c.run()
